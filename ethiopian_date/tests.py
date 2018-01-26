29259d0cb9e6c1c811a9e3f21f183dce85357ca2
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-01-26 15:06:06 -0500

    Updated tests

--- ethiopian_date/tests.py
+++ ethiopian_date/tests.py
@@ -12,9 +12,9 @@ class TestEthiopianCalendar(unittest.TestCase):
     def test_gregorian_to_ethiopian(self):
 
         conv = EthiopianDateConverter.to_ethiopian
-        self.assertEqual(conv(1982, 11, 21), '1975-3-12')
-        self.assertEqual(conv(1941, 12, 7), '1934-3-28')
-        self.assertEqual(conv(2010, 12, 22), '2003-4-13')
+        self.assertEqual(conv(1982, 11, 21), (1975, 3, 12))
+        self.assertEqual(conv(1941, 12, 7), (1934, 3, 28))
+        self.assertEqual(conv(2010, 12, 22), (2003, 4, 13))
 
     def test_ethiopian_to_gregorian(self):
 
